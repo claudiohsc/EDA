349887e71e3e574992900c28fa4b7a48829144ab
CLaudio Henrique <claudio885henrique@gmail.com>
2023-09-19 11:01:07 -0300

    add arquivos

---
+++ labs/tokenizacao.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int word2vec(char *pNomeTxtEntrada){
+...
+}
+
+
+int main(){
+     ...
+     if (!word2vec(NomeArquivo))
+
+         printf("Erro na geração do vocábulo!");
+    ...     
+}
